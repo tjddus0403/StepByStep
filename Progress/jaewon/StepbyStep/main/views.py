df048ec0a926450263535408f714f24d0cad6556
jaewon1778 <jeawoun2001@gmail.com>
2022-11-12 21:50:57 +0900

    qr_ver_backup

---
+++ Progress/jaewon/StepbyStep/main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics
+
+#import sys
+#import os
+#sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def detect(request):
+    return render(request, 'main/detect.html')
+
+def qr(request):
+    return render(request, 'main/qr.html')
+def url(request):
+    return render(request, 'main/url.html')
